701264346d93b077ee904a8bf3d814de68dd0298
Cristy Almonte <44328476+cristyalmonte@users.noreply.github.com>
2025-06-20 01:08:18 -0400

    fix: use python3 to run pip and mkdocs

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Python dependencies
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Build the MkDocs site
-mkdocs build --strict 
+python3 -m mkdocs build --strict 

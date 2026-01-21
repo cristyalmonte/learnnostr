fb15ffee81bcddf128e349cf0d50aa0d8b2b64df
Cristy Almonte <44328476+cristyalmonte@users.noreply.github.com>
2026-01-21 12:42:06 -0500

    Fix Vercel build: Add --break-system-packages flag to pip install

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install Python dependencies
-python3 -m pip install -r requirements.txt
+# Install Python dependencies with --break-system-packages for Vercel
+python3 -m pip install --break-system-packages -r requirements.txt
 
 # Build the MkDocs site
 python3 -m mkdocs build 
